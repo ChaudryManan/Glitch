65d0d0b20c00785424c17dfab174144bbf71c1f2
ChaudryManan <manankhn81@gmail.com>
2025-05-11 13:18:24 +0500

    fixing cors

--- src/app.js
+++ src/app.js
@@ -3,25 +3,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./Routes/user.Routes.js";
 const app = express()
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://full-stack-website-theta.vercel.app"
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: "https://full-stack-website-theta.vercel.app",
+  credentials: true
+}));
 
   
 app.use(express.json({limit:"16kb"}))
