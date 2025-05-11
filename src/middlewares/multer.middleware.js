54f083a920c52dc4782e5d35011d8a7a06f37251
ChaudryManan <manankhn81@gmail.com>
2025-05-11 17:31:54 +0500

    reset multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,21 +1,12 @@
-import multer from "multer";
-import fs from "fs";
-import path from "path";
-
-// Path matches NEW folder name "learn"
-const uploadDir = path.join("C:", "Users", "Usman", "Desktop", "learn", "upload");
-
-// Auto-create directory
-fs.mkdirSync(uploadDir, { recursive: true });
-
+import multer from "multer"
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix);
-  }
-});
-
-export const upload = multer({ storage: storage });
+    destination: function (req, file, cb) {
+      cb(null,  'C:\\Users\\Usman\\Desktop\\learn\\upload')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  export const upload = multer({ storage: storage })
